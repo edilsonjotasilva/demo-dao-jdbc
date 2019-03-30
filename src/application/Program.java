8345f9b61de4527a0cffc7964afca0255769536b
edisonSilva <edilsonjotasilva@gmail.com>
2019-03-30 17:36:18 -0300

    Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	Department obj = new Department(1, "books");
 	System.out.println(obj);
-
+//	Date data = new Date();
+	Seller seller = new Seller(21, "Edilson", "edilson@gmail.com", new Date(), 3500.0, obj);
+	System.out.println(seller);
 	}
 
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -10,28 +10,22 @@ public class Department implements Serializable {
 	
 	public Department() {	
 	}
-
 	public Department(Integer id, String name) {	
 		this.id = id;
 		this.name = name;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -39,7 +33,6 @@ public class Department implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,7 +49,6 @@ public class Department implements Serializable {
 			return false;
 		return true;
 	}
-
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
